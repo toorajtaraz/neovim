ab35bbfe5a0a9a779ad6dadbe7392de65bfa2ed1
Tooraj Taraz <tooraj.info@gmail.com>
2023-01-13 22:56:39 +0330

    adding diag float

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,15 +2,15 @@ local lsp = require('lsp-zero')
 lsp.preset('recommended')
 
 lsp.ensure_installed({
-	'rust_analyzer',
-	'jedi_language_server',
-	'sumneko_lua',
-	'clangd',
-	'cmake',
-	'texlab'
+    'rust_analyzer',
+    'jedi_language_server',
+    'sumneko_lua',
+    'clangd',
+    'cmake',
+    'texlab'
 })
 lsp.configure('rust_analyzer', {
-    on_attach=lsp.on_attach,
+    on_attach = lsp.on_attach,
     settings = {
         ["rust-analyzer"] = {
             imports = {
@@ -30,11 +30,21 @@ lsp.configure('rust_analyzer', {
         }
     }
 })
-lsp.on_attach(function (client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-	vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
 end)
 lsp.setup()
 
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
+vim.diagnostic.config({
+    virtual_text = false,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
+})
+
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false, scope="cursor"})]]
